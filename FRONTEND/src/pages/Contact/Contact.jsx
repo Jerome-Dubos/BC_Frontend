83afdae59f98e7a02d36e6155585a3fad0ecd65d
Jerome Dubos <jeromedubos67@gmail.com>
2025-07-26 11:14:38 +0200

    Finition Contact

--- FRONTEND/src/pages/Contact/Contact.jsx
+++ FRONTEND/src/pages/Contact/Contact.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ContactForm from "../../components/Contact/ContactForm";
+import ContactInfo from "../../components/Contact/ContactInfo";
 import "./Contact.css";
 
 const Contact = () => {
@@ -7,6 +8,8 @@ const Contact = () => {
     <div className="contact-page">
       <div className="contact-container">
         <ContactForm />
+        <div className="contact-separator"></div>
+        <ContactInfo />
       </div>
     </div>
   );
